89bfa4c2843df66156d02652e43dea9bd24fba79
Javaria Brascom <javariab17@gmail.com>
2020-09-09 16:51:59 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
 # Write your solution here
 puts "hey whats the time"
 current_time = Time.now 
-if current_time == even?
+if current_time.to_i == even?
   puts "Even!"
-else current_time == odd?
+else current_time.to_i == odd?
   puts "Odd!"
 end
